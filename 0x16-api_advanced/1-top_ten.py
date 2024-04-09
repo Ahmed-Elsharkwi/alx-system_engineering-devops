0e2b0d6d27b6b9b105d40c63abceb3542edd7e6a
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-04-09 19:15:19 +0200

    mm

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Get the top ten titles of posts
+"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """ return the number of subscribers """
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        dic = response.json().get("data")
+        posts = dic["children"]
+        counter = 0
+        for dict in posts:
+            if counter < 10:
+                print(dict["data"]["title"])
+                counter += 1
+
+    else:
+        return None
+top_ten("programming")
