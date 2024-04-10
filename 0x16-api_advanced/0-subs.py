fe64fcef124a108f7a10678e904cbb45a0919f48
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-04-10 10:51:20 +0200

    MM

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,8 +13,8 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    response = requests.get(url, headers=headers)
-    print("OK")
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         dic = response.json().get("data")
         return dic["subscribers"]
